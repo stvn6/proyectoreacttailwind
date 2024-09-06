9f4627443da27ae7ac0f008c3af3e8683833db4c
stvn6 <165041054+stvn6@users.noreply.github.com>
2024-09-06 04:45:07 -0400

    06-09-2024

--- proyectoreacttail/src/components/Header/Header.jsx
+++ proyectoreacttail/src/components/Header/Header.jsx
@@ -3,10 +3,10 @@ import Button from  "../Button/Button.jsx"
 
 const Nav = () => {
     let Links =[
-        {name: "HOME", link: "/"},
-        {name: "SERVICE", link: "/"},
-        {name: "ABOUT", link: "/"},
-        {name: "CONTACT", link: "7"},
+        {name: "HOME", link: "/home"},
+        {name: "SERVICE", link: "/service"},
+        {name: "ABOUT", link: "/about"},
+        {name: "CONTACT", link: "/contact"},
     ]
     let [open, setOpen] = useState(false);
     return (
@@ -22,9 +22,9 @@ const Nav = () => {
                 <div onClick={()=>setOpen(!open)} className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden">
                     <ion-icon name={open ? "close-outline":"menu-outline"}></ion-icon>
                 </div>
-                <ul className="md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in">
+                <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? 'top-20 ' : 'top-[-490px]'}`}>
                     {
-                        Links.map((link)=>(
+                        Links.map((link) => (
                             <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
                                 <a href={link} className="text-gray-800 hover:text-gray-400 duration-500">{link.name}</a>
                             </li>
